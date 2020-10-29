18fe77455e24ea6ad9be0a97ec821a1741a0f381
Balázs Patrícia <37014240+balapatricia@users.noreply.github.com>
2020-10-29 19:47:32 +0100

    Add files via upload

---
+++ 2020 ősz/zh.Gyakorlas/zh.Gyakorlas/Ko.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zh.Gyakorlas
+{
+    class Ko
+    {
+        static void Main(string[] args)
+        {
+
+            IDictionary<String, IList<String>> dict = new SortedDictionary<String, IList<String>>();
+
+            string line;
+            string[] data;
+
+            while ((line = Console.ReadLine()) != null)
+            {
+                data = line.Split(';');
+                if ((data[1] == "ko" && data[3] == "ollo") ||
+                    (data[1] == "ollo" && data[3] == "papir") ||
+                    (data[1] == "papir" && data[3] == "ko"))
+                {
+                    IList<String> value;
+                    if (!dict.TryGetValue(data[0], out value))
+                    {
+                        value = new List<String>();
+                        dict.Add(data[0], value);
+                    }
+                    value.Add(data[1]);
+                }
+                else if ((data[3] == "ko" && data[1] == "ollo") ||
+                    (data[3] == "ollo" && data[1] == "papir") ||
+                    (data[3] == "papir" && data[1] == "ko"))
+                {
+                    IList<String> value;
+                    if (!dict.TryGetValue(data[2], out value))
+                    {
+                        value = new List<String>();
+                        dict.Add(data[2], value);
+                    }
+                    value.Add(data[3]);
+                }
+            }
+            foreach (var item in dict)
+            {
+                Console.WriteLine(item.Key + ": " + String.Join(", ", item.Value));
+            }
+        }
+    }
+}
